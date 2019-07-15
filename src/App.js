8bc8cb8e04f1c36abd6ceb8e1b32733b56b92cb3
Nick Aja <NJAja@midmark.com>
2019-07-15 14:31:10 -0400

    comments

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// Returns the sum of two numbers together
 export const add = (x, y) => x + y
 
+// Returns a string of the sum of subTotal and shipping prepended with $
 export const totalCost = (subTotal, shipping) => `$${add(subTotal, shipping)}`
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import { add, totalCost } from './App'
 
+// Unit Test
 test('add', () => {
   expect(add(1, 2)).toBe(3)
 })
 
+// Integration Test
+// Tests that work together
 test('total cost', () => {
   expect(totalCost(5, 19.99)).toBe('$24.99')
 })
